9245102369dfda0deebf40252b577bde24e4f533
Samir Kak <samir.kak@dataart.com>
2019-04-23 19:44:06 +0300

    Modified ToDoItemDetails.js (Added state and tern).

--- src/containers/TodoItemDetails.js
+++ src/containers/TodoItemDetails.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import { withRouter } from "react-router";
 import {connect} from "react-redux";
+import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
 class TodoItemDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { visibleInput: '' };
+
+  }
 
   render() {
     const { todo } = this.props;
+
     return (
-      <div>{todo.text}</div>
+      <div>     
+      { this.state.visibleInput
+        ? <><Input value={todo.text}/><Button onClick = {() => { this.setState({ visibleInput: !this.state.visibleInput }) }}>Submit</Button></>
+        : <><span>{todo.text}</span><Button onClick = {() => { this.setState({visibleInput: !this.state.visibleInput}) }}>Edit</Button></>
+      }
+      </div>
     );
   }
 }
@@ -14,6 +27,7 @@ class TodoItemDetails extends Component {
 const mapStateToProps = (state,ownProps) => {
   return { todo: state.todos.find((todo)=>{return todo.id === ownProps.match.params.id}) };
 };
+
 
 export default withRouter(connect(mapStateToProps)(TodoItemDetails));
 
